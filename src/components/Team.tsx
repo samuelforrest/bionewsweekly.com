84b5e18406b465c8f842bac17d4c650ce57a5fd2
Samuel Forrest <sam@samuelforrest.me>
2025-06-26 21:57:50 +0100

    added danny image

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -16,10 +16,10 @@ const teamMembers = [
   {
     name: "Danny Mellor",
     role: "Writer",
-    bio: "Danny is inspired by the cell side of Biology. He aspires to become a marine biologist.",
-    subjects: ["Biology", "Psychology", "Mathematics"],
-    avatar: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    specialty: "Cell Biology",
+    bio: "Danny is an aspiring biologist driven by a deep fascination for understanding life and advancing reserach.",
+    subjects: ["Biology", "TBC", "Mathematics"],
+    avatar: "/danny_pfp.jpg",
+    specialty: "Research",
     epqLink: "/epq/danny-mellor-epq.pdf",
     linkedinLink: "https://linkedin.com/in/danny-mellor"
   },
